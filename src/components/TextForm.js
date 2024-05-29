26511c5c025f48457aeadf5de2014093c2185e30
Divyang Surani <105037987+divyangsurani@users.noreply.github.com>
2024-05-29 17:04:19 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -144,20 +144,20 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? '' : 'white', color: props.mode === 'dark' ? '  ' : '#042743' }} id="myBox" rows="8"></textarea>
                 </div>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={convertToSentenceCase}>Convert to Sentence Case</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={encodeToBase64}>Encode to Base64</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={extractNumbers}>Extract Numbers</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={extractLinks}>Extract Links</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={extractText}>Extract Text</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={listenNow}>Listen Now</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={removeWhiteSpace}>Remove White Space</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={removeSpecialCharacters}>Remove Special Characters</button>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={reverseText}>Reverse Text</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-8" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-8" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-8" onClick={handleClearClick}>Clear Text</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-8" onClick={handleCopy}>Copy Text</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-8" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-8" onClick={convertToSentenceCase}>Convert to Sentence Case</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-8" onClick={encodeToBase64}>Encode to Base64</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-8" onClick={extractNumbers}>Extract Numbers</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-8" onClick={extractLinks}>Extract Links</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-8" onClick={extractText}>Extract Text</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-8" onClick={listenNow}>Listen Now</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-8" onClick={removeWhiteSpace}>Remove White Space</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-8" onClick={removeSpecialCharacters}>Remove Special Characters</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 hover:scale-" onClick={reverseText}>Reverse Text</button>
                 {listening ? (
                     <button     className="btn btn-danger mx-1 my-1" onClick={handleStopListening}>Stop Listening</button>
                 ) : (
